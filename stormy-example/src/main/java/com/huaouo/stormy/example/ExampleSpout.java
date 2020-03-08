46f56302401b9cb0738650166dde7bc819c251e7
Zhenhua Yang <huaouo@live.com>
2020-03-08 21:55:46 +0800

    add stormy-example module

---
+++ stormy-example/src/main/java/com/huaouo/stormy/example/ExampleSpout.java
@@ -0,0 +1,15 @@
+// Copyright 2020 Zhenhua Yang
+// Licensed under the MIT License.
+
+package com.huaouo.stormy.example;
+
+import com.huaouo.stormy.stream.OutputStreamDeclarer;
+import com.huaouo.stormy.topology.Spout;
+
+public class ExampleSpout implements Spout {
+    
+    @Override
+    public void declareOutputStream(OutputStreamDeclarer declarer) {
+
+    }
+}
